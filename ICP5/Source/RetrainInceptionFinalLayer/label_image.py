1eae30e5401a0eb53094611d9afe315c94d8c370
Anvesh <anveshmandadi821@gmail.com>
2019-02-21 22:52:09 -0600

    ICP5

---
+++ ICP5/Source/RetrainInceptionFinalLayer/label_image.py
@@ -0,0 +1,34 @@
+import tensorflow as tf, sys
+
+#image_path = sys.argv[1]
+image_path = 'caltech101\\data\\validation\\scissors\\image_0001.jpg'
+#image_path = 'caltech101\\data\\validation\\accordion\\image_0030.jpg'
+# image_path = '676728-bigthumbnail.jpg'
+
+# Read in the image_data
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line
+               in tf.gfile.GFile("caltech101/data/output_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("caltech101/data/output_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+    # Feed the image_data as input to the graph and get first prediction
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+    predictions = sess.run(softmax_tensor, \
+                           {'DecodeJpeg/contents:0': image_data})
+
+    # Sort to show labels of first prediction in order of confidence
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        print('%s (score = %.5f)' % (human_string, score))
