19e050d3a471a5f5ab2f003deeceeee8a4daff45
Anvesh <anveshmandadi821@gmail.com>
2019-04-18 23:36:04 -0500

    ICP10

---
+++ ICP'S/ICP10/Source/Image_Classification/IPSettings.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by pradyumnad on 19/07/15.
+  */
+object IPSettings {
+
+  val PATH = "data/"
+  val INPUT_DIR = PATH + "train"
+  val TEST_INPUT_DIR = PATH + "test2"
+  val FEATURES_PATH = PATH + "model/features"
+
+  val DecisionTree = PATH + "model/DTreeModel"
+
+  val KMEANS_PATH = PATH + "model/clusters"
+  val HISTOGRAM_PATH = PATH + "model/histograms"
+  val RANDOM_FOREST_PATH = PATH + "model/nbmodel"
+
+  val Naive_Bayes = PATH + "model/NaiveModel"
+
+}
